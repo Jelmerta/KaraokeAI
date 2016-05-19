30595ceabdf3ab849ba21ad2f189f0085ae5b3a0
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-19 23:06:49 +0200

    data

--- vocalDetectionTraining.py
+++ vocalDetectionTraining.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 def main():
 	mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-	print mnist.test
+	print mnist.test._images
 	
 	sess = tf.InteractiveSession()
 #	batch = batchGenerator.getBatch("features/input", "features/output", 32)
