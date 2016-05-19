b09db8cec4c23060c96f3cfc486e7a720e12d04f
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-19 22:52:47 +0200

    data

--- vocalDetectionTraining.py
+++ vocalDetectionTraining.py
@@ -47,14 +47,14 @@ def main():
 	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	sess.run(tf.initialize_all_variables())
-	for i in range(20000):
-		batch = mnist.train.next_batch(50)
-		if i%100 == 0:
-			train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
-			print("step %d, training accuracy %g"%(i, train_accuracy))
-		train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+	# for i in range(20000):
+		# batch = mnist.train.next_batch(50)
+		# if i%100 == 0:
+			# train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
+			# print("step %d, training accuracy %g"%(i, train_accuracy))
+		# train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-	print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+	# print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 	
 def weight_variable(shape):
 	initial = tf.truncated_normal(shape, stddev=0.1)
