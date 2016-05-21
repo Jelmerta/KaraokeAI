4dea451f79e58a16d34490cbd0a686636107e50e
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-21 16:58:26 +0200

    k

--- vocalDetectionTraining.py
+++ vocalDetectionTraining.py
@@ -5,27 +5,27 @@ def main():
 	sess = tf.InteractiveSession()
 	bg = batchGenerator.batchGenerator("../features/input", "../features/output")
 	
-	x = tf.placeholder(tf.float32, shape=[None, 13]) # 13
+	x = tf.placeholder(tf.float32, shape=[None, 650]) # 13
 	y_ = tf.placeholder(tf.float32, shape=[None, 2]) # 2
 	
-	W_conv1 = weight_variable([1, 13, 1, 32]) # was 5 5 1 32
+	W_conv1 = weight_variable([5, 5, 1, 32]) # was 5 5 1 32
 	b_conv1 = bias_variable([32])
 	
-	x_image = tf.reshape(x, [-1,1,13,1]) # was 28/28
+	x_image = tf.reshape(x, [-1,50,13,1]) # was 28/28 (could be 50/13)
 	
 	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = max_pool_2x2(h_conv1)
 	
-	W_conv2 = weight_variable([1, 13, 32, 64]) # was 5 5
+	W_conv2 = weight_variable([5, 5, 32, 64])
 	b_conv2 = bias_variable([64])
 
 	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 	h_pool2 = max_pool_2x2(h_conv2)
 	
-	W_fc1 = weight_variable([7 * 7 * 64, 1024])
+	W_fc1 = weight_variable([12 * 3 * 64, 1024])
 	b_fc1 = bias_variable([1024])
 
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 12*3*64])
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 	
 	sess.run(tf.initialize_all_variables())
@@ -39,12 +39,15 @@ def main():
 	y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 	
 	cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+	#cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logit, y_))
+	#cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y_conv, 1e-10, 1.0)))
+	# use one of these if doesn't work
 	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	sess.run(tf.initialize_all_variables()) #one of these init not ne
 	for i in range(20000):
-		batch = bg.getBatch(0, 32)
+		batch = bg.getBatch(0, 64)
 		print batch
 		if i%100 == 0:
 			train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
