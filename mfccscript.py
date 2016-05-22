d4828126e7281e1f50fc9a3aca5623e0c66e87a4
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-22 23:46:42 +0200

    filesize

--- mfccscript.py
+++ mfccscript.py
@@ -9,6 +9,8 @@ import numpy as np
 USE_HDF5 = 0
 if USE_HDF5:
 	import h5py
+	
+print H5_VERS_INFO
 
 '''Parameters are :
 - CepsIgnoreFirstCoeff (default=1): 0 means to keep the first cepstral coeffcient, 1 means to ignore it
