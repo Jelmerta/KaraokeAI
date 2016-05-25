2c33baf570656d854d83ab5e0df9bc0d8684e9d9
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-25 06:34:10 +0200

    filesize

--- vocalDetectionTraining.py
+++ vocalDetectionTraining.py
@@ -22,10 +22,10 @@ def main():
 	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 	h_pool2 = max_pool_2x2(h_conv2)
 	
-	W_fc1 = weight_variable([12 * 4 * 64, 1024])
+	W_fc1 = weight_variable([13 * 4 * 64, 1024])
 	b_fc1 = bias_variable([1024])
 
-	h_pool2_flat = tf.reshape(h_pool2, [-1, 12*4*64])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 13*4*64])
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 	
 	keep_prob = tf.placeholder(tf.float32)
