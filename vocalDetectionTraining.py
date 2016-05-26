2b9f910a1967a393d7781d07aa8d4fee3de13aaf
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-26 23:10:35 +0200

    filesize

--- vocalDetectionTraining.py
+++ vocalDetectionTraining.py
@@ -28,7 +28,7 @@ def main():
 	h_pool2_flat = tf.reshape(h_pool2, [-1, 13*4*64])
 	h_fc1_drop = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 	
-	# keep_prob = tf.placeholder(tf.float32)
+	keep_prob = tf.placeholder(tf.float32)
 	# h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob) #should dropout happen first?
 	
 	W_fc2 = weight_variable([1024, 2])
