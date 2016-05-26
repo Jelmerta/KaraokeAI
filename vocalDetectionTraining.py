ee1bbd1ede7f380e3a1acd963713ec4e8f813316
Jelmerta <jelmer_alphenaar@hotmail.com>
2016-05-26 23:25:09 +0200

    filesize

--- vocalDetectionTraining.py
+++ vocalDetectionTraining.py
@@ -11,7 +11,7 @@ def main():
 	W_conv1 = weight_variable([5, 5, 1, 32])
 	b_conv1 = bias_variable([32])
 	
-	x_image = tf.reshape(x, [-1,50,13,1]) # (could be 50/13)
+	x_image = tf.reshape(x, [-1,50,13,1])
 	
 	h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 	h_pool1 = max_pool_2x2(h_conv1)
@@ -29,18 +29,18 @@ def main():
 	h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 	
 	keep_prob = tf.placeholder(tf.float32)
-	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob) #should dropout happen first?
+	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 	
 	W_fc2 = weight_variable([1024, 2])
 	b_fc2 = bias_variable([2])
 
 	y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 	
-	#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+	cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+	#cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
 	#cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y_conv, 1e-10, 1.0)))
 	# use one of these if doesn't work
-	train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+	train_step = tf.train.AdamOptimizer(1e-5).minimize(cross_entropy)
 	correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	saver = tf.train.Saver()
